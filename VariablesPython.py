adb7ecaac767643d97fd194bbd236674b5e2164c
Salvador CR <salvador.cartagena@gmail.com>
2023-08-24 14:43:37 -0500

    version 0.1

---
+++ VariablesPython.py
@@ -0,0 +1,25 @@
+# Python tiene 4 tipos Primitivos:
+# strings>>Arreglo de caracteres que forman cadenas para formar un mensaje.Se pueden crear usando comillas simples,dobles o triples.
+nombre = "Salvador"
+apellido = 'Cartagena'
+cursos = """
+1. Web Development Fundamentals
+2. Fundamentos de Programacion con Python
+3. Fundamentos Git
+4. Bases de Datos
+5. Procesamiento de Datos
+"""
+print (f"Nombre: {nombre} {apellido}")
+print (f"Cursos: {cursos}")
+
+#Enteros>>(int) se utilizan para representar datos numericos, solo numeros enteros, tanto positivo como negativo.
+year = 2023
+dia = 24
+edad = 39
+temperatura = -15
+print (year , dia , edad , temperatura, sep = "/")
+# Flotantes>>
+
+# Booleanos>>
+# cadenas>>
+
